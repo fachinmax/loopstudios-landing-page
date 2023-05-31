d9614e01b8aa8ac5febd150f8bee1770049e5aa2
fachinmax <maxfachin98@gmail.com>
2023-05-31 13:35:24 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('#menubar-button')
+const navbar = document.querySelector('#header-nav')
+const logo = document.querySelector('.logo')
+
+btn.addEventListener('click', e => {
+    let menunavButton = e.target
+    if(menunavButton.getAttribute('aria-pressed') === 'false') { 
+        menunavButton.setAttribute('aria-pressed', 'true')
+        menunavButton.setAttribute('aria-expanded', 'true')
+        navbar.setAttribute('data-visual', 'true')
+        logo.classList.toggle('position-fixed')
+    } else {
+        menunavButton.setAttribute('aria-pressed', 'false')
+        menunavButton.setAttribute('aria-expanded', 'false')
+        navbar.setAttribute('data-visual', 'false')
+        logo.classList.toggle('position-fixed')
+    }
+});
